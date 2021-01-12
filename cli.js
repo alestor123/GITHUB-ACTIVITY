145f45bf67872934f0518853525e9f6e9880d2d7
alestor123 <alestoraldous@gmail.com>
2021-01-12 13:48:53 +0530

    added token

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 var gha = require('./App');
 (async () => {
-(await gha(process.argv[2] || 'github')).map(event => console.log(event))
+(await gha(process.argv[2] || 'github',process.argv[3])).map(event => console.log(event))
 })();
